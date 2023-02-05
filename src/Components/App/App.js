0521bf335364b04b6ba437d6c9c044be9581eb5e
Tyler <willdev0110@gmail.com>
2023-02-05 12:30:49 -0700

    add searchResults state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,21 +1,36 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Searchbar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
 
-function App() {
-  return (
-    <div>
-      <h1>
-        Ja<span className="highlight">mmm</span>ing
-      </h1>
-      <div className="App">
-        <Searchbar />
-        <div className="App-playlist">
-          <Searchresults />
-          <Playlist />
+class App extends React.Component {
+  constructor(props) {
+    this.state = {
+      searchResults: [
+        { name: "name" },
+        { artist: "artist" },
+        { album: "album" },
+        { id: "id" },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <Searchbar />
+          <div className="App-playlist">
+            <Searchresults searchResults={this.state.searchResults} />
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
