d831a7001cacb9f34395680f01dc41e56ace3411
Tyler <willdev0110@gmail.com>
2023-02-05 18:33:12 -0700

    add playlistTracks prop to Tracklist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -10,7 +10,7 @@ class Playlist extends React.Component {
     return (
       <div className="Playlist">
         <input defaultValue={"New Playlist"} />
-        {/* <Tracklist /> */}
+        <Tracklist tracks={this.props.playlistTracks} />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
