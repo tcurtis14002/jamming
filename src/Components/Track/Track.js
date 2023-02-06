4dc99a625abdfe69c59212c8f2195ea4b6ff033a
Tyler <willdev0110@gmail.com>
2023-02-05 17:34:40 -0700

    change import/export syntax,  terney to if/else

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import './Track.css';
+import React from "react";
+import "./Track.css";
 
-export class Track extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+class Track extends React.Component {
   renderAction() {
-    <button className='Track-action'> {this.props.isRemoval ? '-' : '+'}</button>
+    if (this.props.isRemoval) {
+      return <button className="Track-action">-</button>;
+    } else {
+      return <button className="Track-action">+</button>;
+    }
   }
 
   render() {
     return (
       <div className="Track">
-  <div className="Track-information">
-    <h3><!-- track name will go here --></h3>
-    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
-  </div>
-  <button className="Track-action"><!-- + or - will go here --></button>
-</div>
-    )
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>
+            {this.props.track.artist} | {this.props.track.album}
+          </p>
+        </div>
+        {this.renderAction()}
+      </div>
+    );
   }
 }
+
+export default Track;
