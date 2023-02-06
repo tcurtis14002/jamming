ca7ae0e13993064c9ee53a0781ab38031f83d5d0
Tyler <willdev0110@gmail.com>
2023-02-05 18:33:49 -0700

    add constructor

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,11 +2,29 @@ import React from "react";
 import "./Track.css";
 
 class Track extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
+      return;
+    }
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
   renderAction() {
     if (this.props.isRemoval) {
       return <button className="Track-action">-</button>;
     } else {
-      return <button className="Track-action">+</button>;
+      return (
+        <button className="Track-action" onClick={this.addTrack}>
+          +
+        </button>
+      );
     }
   }
 
