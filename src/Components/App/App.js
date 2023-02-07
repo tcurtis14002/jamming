ee15d53b126f0a167d455a271ccd8330969ab017
Tyler <willdev0110@gmail.com>
2023-02-07 10:08:42 -0700

    add addTrack and removeTrack method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -51,19 +51,23 @@ class App extends React.Component {
       ],
     };
 
-    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
-    if (
-      this.state.playlistTracks.find(
-        (savedTrack) => savedTrack.id === track.id
-      )
-    ) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
       return;
     }
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
   }
 
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter((currentTrack) => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
   }
 
   render() {
@@ -82,6 +86,7 @@ class App extends React.Component {
             <Playlist
               playlistTracks={this.state.playlistTracks}
               playlistName={this.state.playlistName}
+              onRemove={this.removeTrack}
             />
           </div>
         </div>
