8bf63386daa3492550494f4d05a650cb0e4a251c
Tyler <willdev0110@gmail.com>
2023-02-07 10:59:47 -0700

    add handleNameChange, pass methods to Tracklist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -5,16 +5,28 @@ import "./Playlist.css";
 class Playlist extends React.Component {
   constructor(props) {
     super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
   }
+
+  handleNameChange(e) {
+    this.props.onNameChange(e.target.value);
+  }
+
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue={"New Playlist"} />
+        <input
+          defaultValue={"New Playlist"}
+          onChange={this.handleNameChange}
+        />
         <Tracklist
           tracks={this.props.playlistTracks}
           onRemove={this.props.removeTrack}
+          isRemoval={true}
         />
-        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        <button className="Playlist-save" onClick={this.props.onSave}>
+          SAVE TO SPOTIFY
+        </button>
       </div>
     );
   }
